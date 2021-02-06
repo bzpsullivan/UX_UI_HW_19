ca86b1ed66a8790bd827df0bd15610ab79f0b346
bzpsullivan <68710591+bzpsullivan@users.noreply.github.com>
2021-02-06 00:12:30 -0700

    All Iterations Done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 console.log("Hello, World.");
+$(document).ready(function(){
+    $("#flip").click(function(){
+      $("#panel").slideDown("slow");
+    });
+  });
+  $(document).ready(function(){
+    $("#flipUp").click(function(){
+      $("#panel").slideUp("slow");
+    });
+  });
